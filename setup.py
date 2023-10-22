e65a077ec52572fb71408e289c6cf50ac1e02326
Max Lamparth <72386596+maxlampe@users.noreply.github.com>
2023-10-21 17:52:54 -0700

    fix: Included requirements in setup.py.

--- setup.py
+++ setup.py
@@ -6,14 +6,19 @@ from setuptools import setup, find_packages
 # # run pip-compile requirements.in to update requirements.txt
 # subprocess.run(["pip-compile", "requirements.in"])
 
+
 # # Read in the requirements.txt file
 # with open('requirements.txt') as f:
 #     requirements = f.read().splitlines()
+def read_requirements():
+    with open("requirements.txt", "r") as file:
+        return [line.strip() for line in file if line.strip()]
+
 
 setup(
     name="collaborative_experiments",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=requirements,
+    install_requires=read_requirements(),
 )
