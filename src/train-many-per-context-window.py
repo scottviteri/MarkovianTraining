bec02357fc063c8855d1796c0265f0d81d1d3cb8
Scott Viteri <scottviteri@gmail.com>
2023-12-08 21:58:37 +0000

    Fixed typo into model wandb name
    Former-commit-id: 0239a4053254470d3d854d9e23a996df5b7208ee

--- src/train-many-per-context-window.py
+++ src/train-many-per-context-window.py
@@ -62,7 +62,7 @@ def train():
     # todo add flag for ld
     lora_string = "L" if cfg.do_lora else "nL"
     if run is not None:
-        run.name = fl"ld_b4_{lora_string}{cfg.model_name[:4]}_lr{cfg.lr}_rao{cfg.tok_p_loss}/{cfg.tok_p_action}/{cfg.tok_p_obs}_bs{cfg.batch_size}_nb{cfg.num_batches}"
+        run.name = f"ld_b4_{lora_string}{cfg.model_name[:4]}_lr{cfg.lr}_rao{cfg.tok_p_loss}/{cfg.tok_p_action}/{cfg.tok_p_obs}_bs{cfg.batch_size}_nb{cfg.num_batches}"
 
     with open(f"{cfg.save_dir}/{cfg.model_name}_training_info.txt", "w") as f:
         print(f)
