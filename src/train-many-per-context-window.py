8fc44cc5d28b1925acc387f0f6b304ae70832649
Scott Viteri <scottviteri@gmail.com>
2023-12-08 19:04:07 +0000

    Install instructions in one line of comment

--- src/train-many-per-context-window.py
+++ src/train-many-per-context-window.py
@@ -1,6 +1,5 @@
-# pip install transformers datasets==2.14.6 torchtyping==0.1.4
-# pip install peft einops apache_beam==2.51.0 matplotlib wandb
-# pip install -U flash-attn --no-build-isolation
+# pip install transformers datasets==2.14.6 torchtyping==0.1.4 && pip install peft einops apache_beam==2.51.0 matplotlib wandb && pip install -U flash-attn --no-build-isolation
+# huggingface-cli login
 
 import torch
 from tqdm import tqdm
