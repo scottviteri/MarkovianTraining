fix: Added missing loss for prompt for actual loss calculation,

src/rao_generator.py
@@ -149,8 +149,9 @@ class RaoGenerator:
             with torch.no_grad():
                 # prediction = causal_lm(
                 #    torch.cat((action, true_obs), dim=-1)[:, -(self._cfg.tok_p_loss + self._cfg.tok_p_action):]
-                #)
-                prediction = causal_lm(torch.cat((action, true_obs), dim=-1))
+                # )
+                # FIXME: Why is there no loss?
+                prediction = causal_lm(torch.cat((low_loss, action, true_obs), dim=-1))
                 predicted_logits = prediction.logits[
                     :, -self._cfg.tok_p_obs - 1 : -1, :
                 ]