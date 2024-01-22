60394dfb1837f7e20a5509171a10ced153a1ca01
Scott Viteri <scottviteri@gmail.com>
2024-01-22 14:30:52 -0800

    Removed unnecessary variable names in train_autoregressive

--- src/train_autoregressive.py
+++ src/train_autoregressive.py
@@ -12,26 +12,12 @@ from src.types_and_utilities import log_and_print_info
 
 
 def train_autoregressive(cfg):
-    causal_lm = cfg.causal_lm
     loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(causal_lm.parameters(), lr=cfg.lr)
-    #itr_ds = load_dataset(
-    #    cfg.dataset_name, cfg.task_name, split="train", streaming=True
-    #)
-    #ds_tokenized = map(
-    #    lambda x: causal_lm_tokenizer(x["text"], return_tensors="pt")["input_ids"].to(
-    #        cfg.device
-    #    ),
-    #    itr_ds,
-    #)
-    #pure_obs = get_pure_obs(cfg.batch_size, cfg.tok_p_obs, cfg.device, ds_tokenized)
-    #obs_ds = take(cfg.num_batches, pure_obs)
-    obs_ds = cfg.dataloader
-    # Initialize the list to store the losses
+    optimizer = torch.optim.SGD(cfg.causal_lm.parameters(), lr=cfg.lr)
     losses = []
-    for batch_index, obs in tqdm(enumerate(obs_ds), total=cfg.num_batches):
+    for batch_index, obs in tqdm(enumerate(cfg.dataloader), total=cfg.num_batches):
         optimizer.zero_grad()
-        logits = causal_lm(obs).logits[:, :-1, :]
+        logits = cfg.causal_lm(obs).logits[:, :-1, :]
         loss = loss_fn(
             input=einops.rearrange(
                 logits,
