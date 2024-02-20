1f5358663ec38d1f58e8e92cc41ac661ca019bc5
Scott Viteri <scottviteri@gmail.com>
2024-02-20 22:18:30 +0000

    Fixed optimizer in sharded version

--- src/train.py
+++ src/train.py
@@ -17,13 +17,19 @@ import functools
 import torch.distributed as dist
 import pytorch_lightning as pl
 from pytorch_lightning.strategies import FSDPStrategy
-from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy, size_based_auto_wrap_policy
 from torch.distributed.fsdp import MixedPrecision, FullyShardedDataParallel
+from torch.distributed.fsdp.wrap import (
+   enable_wrap,
+   wrap,
+)
 
 from src.training_types import *
 from src.utilities import extend_initial_config, log_and_print_info
 from src.utilities import create_run_name, multi_print
 from src.config_examples import configs 
+
+from transformers.models.gptj.modeling_gptj import GPTJBlock
 
 
 from src.evaluate_via_gpt import evaluate_via_gpt
@@ -129,9 +135,9 @@ def sample(cfg, prev_action, prev_obs, observation):
                 )[:, -cfg.tok_p_action :]
         return action_candidates
 
-def update_weights(cfg, optimizer, prev_action, prev_obs, action, obs):
+def update_weights(cfg, prev_action, prev_obs, action, obs):
     training_cfg = cfg.training_cfg
-    optimizer.zero_grad()
+    cfg.optimizer.zero_grad()
     loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
     cfg.causal_lm.train()
     with autocast(cache_enabled=False, dtype=torch.bfloat16 if cfg.model_name in ["llama", "mistral"] else torch.float16):
@@ -188,7 +194,7 @@ def update_weights(cfg, optimizer, prev_action, prev_obs, action, obs):
 
     if not isinstance(cfg.debug, NoWeightUpdates):
         aggregate_loss.backward()
-        optimizer.step()
+        cfg.optimizer.step()
     
     if training_cfg.train_O_given_prev_O: return aggregate_loss, None, None
     loss_tensors = prev_action_tensor, prev_observation_tensor, action_tensor, obs_tensor
@@ -204,7 +210,7 @@ def log_and_save(cfg, batch_index, prev_action, prev_obs, action, obs, is_guidan
         log_wandb(cfg, batch_index, aggregate_loss, losses)
         log_print_losses(cfg, batch_index, aggregate_loss, losses)
 
-def trainer(cfg, optimizer):
+def trainer(cfg):
     state = [default_action(cfg), 0, None]
 
     def update(datapt_pair):
@@ -225,7 +231,7 @@ def trainer(cfg, optimizer):
             action = prev_action
         else:
             action = sample(cfg, prev_action, prev_obs, obs) 
-        aggregate_loss, loss_tensors, losses = update_weights(cfg, optimizer, prev_action, prev_obs, action, obs)
+        aggregate_loss, loss_tensors, losses = update_weights(cfg, prev_action, prev_obs, action, obs)
         log_and_save(cfg, batch_index, prev_action, prev_obs, action, obs, "Action" in datapt, is_first, aggregate_loss, losses)
         state = [action, batch_index + 1, aggregate_loss]
         return
@@ -236,100 +242,99 @@ def trainer(cfg, optimizer):
     
     return update, pi
 
-def train_via_update(cfg, optimizer):
+def train_via_update(cfg):
     aggregate_losses = []
-    update, pi = trainer(cfg, optimizer)
+    update, pi = trainer(cfg)
     for datapt_pair in tqdm(cfg.dataset.dataloader, total=cfg.num_batches):
         aggregate_loss = pi()
         if aggregate_loss is not None: aggregate_losses.append(aggregate_loss)
         update(datapt_pair)
     return aggregate_losses
 
-class LitModel(pl.LightningModule):
-    def __init__(self, cfg):
-        super().__init__()
-        print("Dev", self.device)
-        self.cfg = cfg
-        self.cfg.device = self.device
-        self.cfg.action_prefix_tensor = self.cfg.action_prefix_tensor.to(self.device)
-        self.cfg.obs_prefix_tensor = self.cfg.obs_prefix_tensor.to(self.device)
-        self.model = self.cfg.causal_lm
-        self.state = [default_action(self.cfg), 0, None]
+transformer_auto_wrapper_policy = functools.partial(
+    transformer_auto_wrap_policy,
+    transformer_layer_cls={ GPTJBlock, },
+)
 
-    def forward(self, prev_action, prev_obs, obs):
-        return sample(self.cfg, prev_action, prev_obs, obs)
+#class LitModel(pl.LightningModule):
+#    def __init__(self, cfg):
+#        super().__init__()
+#        print("Dev", self.device)
+#        self.cfg = cfg
+#        self.cfg.device = self.device
+#        self.cfg.action_prefix_tensor = self.cfg.action_prefix_tensor.to(self.device)
+#        self.cfg.obs_prefix_tensor = self.cfg.obs_prefix_tensor.to(self.device)
+#        self.model = self.cfg.causal_lm
+#        self.state = [default_action(self.cfg), 0, None]
+#
+#    def forward(self, prev_action, prev_obs, obs):
+#        return sample(self.cfg, prev_action, prev_obs, obs)
+#
+#    def training_step(self, datapt_pair, batch_idx):
+#        #if batch_idx == 0:
+#        #    print(self)
+#        self.cfg.device = self.device
+#        self.cfg.action_prefix_tensor = self.cfg.action_prefix_tensor.to(self.device)
+#        self.cfg.obs_prefix_tensor = self.cfg.obs_prefix_tensor.to(self.device)
+#        prev_datapt, datapt = datapt_pair
+#        is_first = "First" in datapt and datapt["First"]
+#        prev_action, batch_index, _ = self.state
+#        prev_action = prev_action.to(self.device)
+#        prev_obs, obs = prev_datapt["Observation"], datapt["Observation"]
+#        if is_first: 
+#            prev_action = default_action(self.cfg)
+#            log_print_oa(self.cfg, batch_index, prev_action, prev_obs, None, obs, "Action" in datapt, is_first)
+#            self.state = [prev_action, batch_index + 1, None]
+#            return torch.tensor(0.0, device=self.device, requires_grad=True) #self.cfg.causal_lm(torch.zeros((1,1), requires_grad=True)).sum() * 0 #None #torch.tensor(5.0)
+#        # now can assume that prev_datapt contains the question and datapt contains the Answer
+#        if "Action" in datapt: 
+#            action = datapt["Action"]
+#        elif self.cfg.training_cfg.train_O_given_prev_O: 
+#            action = prev_action
+#        else:
+#            action = self.forward(prev_action, prev_obs, obs) 
+#        aggregate_loss, loss_tensors, losses = update_weights(self.cfg, prev_action, prev_obs, action, obs)
+#        log_and_save(self.cfg, batch_index, prev_action, prev_obs, action, obs, "Action" in datapt, is_first, aggregate_loss, losses)
+#        self.state = [action, batch_index + 1, aggregate_loss]
+#        return aggregate_loss 
+#
+#    def configure_optimizers(self):
+#        if self.cfg.optimizer == "sgd":
+#            optimizer = torch.optim.SGD(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)#, momentum=0.01)
+#        elif self.cfg.optimizer == "adam":
+#            optimizer = torch.optim.AdamW(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)
+#        elif self.cfg.optimizer == "rmsprop":
+#            optimizer = torch.optim.RMSprop(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)
+#        return optimizer
+#
+#def pl_train_model(init_cfg):
+#    cfg = extend_initial_config(init_cfg)
+#    if not cfg.load_model:
+#        with open(cfg.path_2_log, "w") as f:
+#            print("")
+#    with open(cfg.path_2_log, "a") as f:
+#        f.write("")
+#    if cfg.wandb and dist.get_rank() == 0: 
+#        wandb.init(
+#            project="collaborative-training-many-per-context-window", 
+#            name=create_run_name(cfg))
+#    model = LitModel(cfg)
+#    trainer = pl.Trainer(num_nodes=1,
+#        max_epochs=1, limit_train_batches=cfg.num_batches, accelerator="cuda", 
+#        devices=2, 
+#        strategy = FSDPStrategy(
+#            sharding_strategy="FULL_SHARD",
+#            auto_wrap_policy = transformer_auto_wrapper_policy,
+#            mixed_precision = MixedPrecision(param_dtype=torch.bfloat16)
+#            ))
+#    trainer.fit(model, cfg.dataset.dataloader)
+#    if cfg.wandb and dist.get_rank() == 0: wandb.finish()
+#
 
-    def training_step(self, datapt_pair, batch_idx):
-        #if batch_idx == 0:
-        #    print(self)
-        self.cfg.device = self.device
-        self.cfg.action_prefix_tensor = self.cfg.action_prefix_tensor.to(self.device)
-        self.cfg.obs_prefix_tensor = self.cfg.obs_prefix_tensor.to(self.device)
-        prev_datapt, datapt = datapt_pair
-        is_first = "First" in datapt and datapt["First"]
-        prev_action, batch_index, _ = self.state
-        prev_action = prev_action.to(self.device)
-        prev_obs, obs = prev_datapt["Observation"], datapt["Observation"]
-        if is_first: 
-            prev_action = default_action(self.cfg)
-            log_print_oa(self.cfg, batch_index, prev_action, prev_obs, None, obs, "Action" in datapt, is_first)
-            self.state = [prev_action, batch_index + 1, None]
-            return torch.tensor(0.0, device=self.device, requires_grad=True) #self.cfg.causal_lm(torch.zeros((1,1), requires_grad=True)).sum() * 0 #None #torch.tensor(5.0)
-        # now can assume that prev_datapt contains the question and datapt contains the Answer
-        if "Action" in datapt: 
-            action = datapt["Action"]
-        elif self.cfg.training_cfg.train_O_given_prev_O: 
-            action = prev_action
-        else:
-            action = self.forward(prev_action, prev_obs, obs) 
-        aggregate_loss, loss_tensors, losses = update_weights(self.cfg, prev_action, prev_obs, action, obs)
-        log_and_save(self.cfg, batch_index, prev_action, prev_obs, action, obs, "Action" in datapt, is_first, aggregate_loss, losses)
-        self.state = [action, batch_index + 1, aggregate_loss]
-        return aggregate_loss 
-
-    def configure_optimizers(self):
-        if self.cfg.optimizer == "sgd":
-            optimizer = torch.optim.SGD(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)#, momentum=0.01)
-        elif self.cfg.optimizer == "adam":
-            optimizer = torch.optim.AdamW(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)
-        elif self.cfg.optimizer == "rmsprop":
-            optimizer = torch.optim.RMSprop(self.cfg.causal_lm.parameters(), lr=self.cfg.lr)
-        return optimizer
-
-def custom_auto_wrap_policy(
-    module: torch.nn.Module,
-    recurse: bool,
-    nonwrapped_numel: int,
-    # Additional custom arguments
-    min_num_params: int = int(1e8),
-) -> bool:
-    return nonwrapped_numel >= min_num_params
-# Configure a custom `min_num_params`
-my_auto_wrap_policy = functools.partial(custom_auto_wrap_policy, min_num_params=int(1e1))
-
-def pl_train_model(init_cfg):
-    cfg = extend_initial_config(init_cfg)
-    if not cfg.load_model:
-        with open(cfg.path_2_log, "w") as f:
-            print("")
-    with open(cfg.path_2_log, "a") as f:
-        f.write("")
-    if cfg.wandb and dist.get_rank() == 0: 
-        wandb.init(
-            project="collaborative-training-many-per-context-window", 
-            name=create_run_name(cfg))
-    model = LitModel(cfg)
-    trainer = pl.Trainer(num_nodes=1,
-        max_epochs=1, limit_train_batches=cfg.num_batches, accelerator="cuda", 
-        devices=2, 
-        strategy = FSDPStrategy(
-            sharding_strategy="FULL_SHARD",
-            #auto_wrap_policy=my_auto_wrap_policy, 
-            auto_wrap_policy = size_based_auto_wrap_policy, 
-            mixed_precision = MixedPrecision(param_dtype=torch.bfloat16)
-            ))
-    trainer.fit(model, cfg.dataset.dataloader)
-    if cfg.wandb and dist.get_rank() == 0: wandb.finish()
+#def custom_auto_wrap_policy(module: torch.nn.Module, recurse: bool, nonwrapped_numel: int, min_num_params: int = 1e8) -> bool:
+#    should_wrap = isinstance(module, GPTJBlock) and nonwrapped_numel >= min_num_params
+#    #print(f"Checking module {module.__class__.__name__}: {'wrapping' if should_wrap else 'not wrapping'}")
+#    return should_wrap
 
 def train_model(init_cfg):
     cfg = extend_initial_config(init_cfg)
@@ -342,21 +347,21 @@ def train_model(init_cfg):
         wandb.init(
             project="collaborative-training-many-per-context-window", 
             name=create_run_name(cfg))
-    #cfg.causal_lm = FullyShardedDataParallel(
-    #    cfg.causal_lm, 
-    #    auto_wrap_policy=size_based_auto_wrap_policy,
-    ##    mixed_precision = MixedPrecision(param_dtype=torch.bfloat16)
-    #)
+    cfg.causal_lm = FullyShardedDataParallel(
+        cfg.causal_lm, 
+        auto_wrap_policy=transformer_auto_wrapper_policy,
+        #mixed_precision = MixedPrecision(param_dtype=torch.bfloat16)
+    )
     print(cfg.causal_lm)
     if cfg.optimizer == "sgd":
-        optimizer = torch.optim.SGD(cfg.causal_lm.parameters(), lr=cfg.lr, momentum=0.01)
+        cfg.optimizer = torch.optim.SGD(cfg.causal_lm.parameters(), lr=cfg.lr)#, momentum=0.01)
     elif cfg.optimizer == "adam":
-        optimizer = torch.optim.Adam(cfg.causal_lm.parameters(), lr=cfg.lr)
+        cfg.optimizer = torch.optim.Adam(cfg.causal_lm.parameters(), lr=cfg.lr)
     elif cfg.optimizer == "rmsprop":
-        optimizer = torch.optim.RMSprop(cfg.causal_lm.parameters(), lr=cfg.lr)
+        cfg.optimizer = torch.optim.RMSprop(cfg.causal_lm.parameters(), lr=cfg.lr)
     else:
-        raise ValueError(f"Unsupported optimizer: {cfg.optimizer}. Please choose either 'sgd' or 'adam'.")
-    train_via_update(cfg, optimizer)
+        raise ValueError(f"Unsupported optimizer: {cfg.optimizer}. Please choose 'sgd', 'adam', or 'rmsprop'.")
+    train_via_update(cfg)
     if cfg.wandb and dist.get_rank() == 0: wandb.finish()
 
 def setup_distributed(backend='nccl', port='12355'):
