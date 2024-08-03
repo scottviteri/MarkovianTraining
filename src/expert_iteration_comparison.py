fcebab88640274bb3e8d7fbc6a7ed2296c179cc1
Scott Viteri <scottviteri@gmail.com>
2024-08-03 01:35:53 +0000

    lora inference mode set to false

--- src/expert_iteration_comparison.py
+++ src/expert_iteration_comparison.py
@@ -17,7 +17,7 @@ def load_mistral_model():
 
     peft_config = LoraConfig(
         task_type="CAUSAL_LM",
-        inference_mode=True,
+        inference_mode=False,
         r=8,
         lora_alpha=16,
         lora_dropout=0.1,
