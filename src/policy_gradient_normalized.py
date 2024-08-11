622df86e59a0d7e17d2fe9418159a54c6bc7ff2d
Scott Viteri <scottviteri@gmail.com>
2024-08-11 01:25:55 +0000

    default 1000 batches (though written as 2000)

--- src/policy_gradient_normalized.py
+++ src/policy_gradient_normalized.py
@@ -410,7 +410,7 @@ def train(use_gsm8k: bool, resume: bool, use_ei: bool):
             "model_learning_rate": 1e-4,
             "batch_size": 6,
             "gradient_accumulation_steps": 8,
-            "num_batches": 1001,
+            "num_batches": 2001,
             "use_ppo": True,
             "ppo_epsilon": 0.2,
             "normalize_loss": True,
